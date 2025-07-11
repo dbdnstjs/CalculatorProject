42cfc17c1e5e53ee6cee191066665655445aec7e
dbdnsdb123 <dbdnsdb123@naver.com>
2025-07-11 21:02:26 +0900

    [feat] add comment

---
+++ src/main/java/org/example/lv3/Calculator.java
@@ -0,0 +1,11 @@
+package org.example.lv3;
+
+public class Calculator {
+
+
+    //enum -> operator 구현
+
+    //getter 구현
+
+    //setter 구현
+}
--- src/main/java/org/example/lv3/Lv3Calculator.java
+++ src/main/java/org/example/lv3/Lv3Calculator.java
@@ -3,5 +3,26 @@ package org.example.lv3;
 public class Lv3Calculator {
     public static void main(String[] args) {
 
+
+        //값 입력
+
+        //연산자 입력
+
+        //값 입력
+
+        //합 출력
+
+        //저장 여부
+
+        //삭제 여부
+        
+        //스트림 + 람다로 조회조건 추가 (큰순 출력, 작은순 출력, 원하는 값 조회, 원하는 범위 값 출력)
+
+        //제네릭으로 실수 구현
+
+        //예외 처리
+
+        //깃
+
     }
 }
